b26d232c0ec1b2320edd0a5721ca92a4fd69a9bd
jctaoo <jctaoo@outlook.com>
2020-08-17 15:22:02 +0800

    chore(改变项目结构): 分离渲染和主线两部分

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,15 @@
 module.exports = {
   pluginOptions: {
     electronBuilder: {
+      mainProcessFile: 'src/lib/main.ts',
       builderOptions: {
         // options placed here will be merged with default configuration and passed to
         // electron-builder
       },
     },
+  },
+  configureWebpack: (config) => {
+    // eslint-disable-next-line no-param-reassign
+    config.entry.app = './src/renderer/main.ts';
   },
 };
